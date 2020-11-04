c1e8841abadfd734944767cd9d0f8c11f60eb466
Albert <kwaafjets2310@gmail.com>
2020-11-04 06:12:17 -0500

    initial commit

---
+++ prog1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	// A program to compute the factorial of a numeber.
+	/*
+	int n, i, factorial;
+	for ( n = 2; n <= 10; n ++ )
+	{
+	factorial = n;
+	for ( i = 2 ; i <= n ; i++ )
+	{
+		factorial *= ( i - 1 );	
+	}
+		printf(" The factorial of %i is %i \n", n, factorial);
+	}
+	*/
+
+
+	// A program to sum the digits of a number. 
+	/*
+	int number, right_digit, sum, numPrint;
+
+	printf ("Enter your number.\n ");
+	scanf ("%i", &number);
+
+	numPrint = number;
+
+	while ( number != 0) 
+	{
+		right_digit = number % 10;
+		sum += right_digit;
+		number = number / 10;
+	}
+
+	printf ("\n");
+	
+	printf ("The sum of %i is %i. Thank You. \n", numPrint, sum);
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;	
+}	
